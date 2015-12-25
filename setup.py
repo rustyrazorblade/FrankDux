44d14ecc1c0518e62ef42999beccc2616cc0147f
Jon Haddad <jon@jonhaddad.com>
2015-12-24 14:31:10 -1000

    frank dux cli is there

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
         name='FrankDux',
         version='0.1',
-        packages=['tests', 'frankdux'],
+        packages=['frankdux'],
         url='https://github.com/rustyrazorblade/FrankDux',
         license='License :: OSI Approved :: BSD License',
         author='Jon Haddad',
