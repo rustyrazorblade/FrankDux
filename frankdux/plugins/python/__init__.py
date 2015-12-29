15155b75231dabe74e09e09e9024d63959cb11ed
Jon Haddad <jon@jonhaddad.com>
2015-12-28 14:32:25 -1000

    writing setup and init files

--- frankdux/plugins/python/__init__.py
+++ frankdux/plugins/python/__init__.py
@@ -17,9 +17,18 @@ def main(frank, output_dir):
     fp.write("\n")
     fp.close()
 
-
-
     # setup.py
     setup = env.get_template("setup.jinja")
+    rendered_setup = setup.render(frank=frank)
+
+    loc = "{}/setup.py".format(output_dir)
+    fp = open(loc, 'w')
+    fp.write(rendered_client)
+    fp.write("\n")
+    fp.close()
+
+    # make sure we've got an empty __init__ so it's a real package
+    loc = "{}/__init__.py".format(output_dir)
+    open(loc, 'w').close()
 
 
