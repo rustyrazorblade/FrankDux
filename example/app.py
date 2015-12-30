e9a793cd59434eb889453f245f8446079b54418f
Jon Haddad <jon@jonhaddad.com>
2015-12-29 18:49:15 -0800

    list of strings

--- example/app.py
+++ example/app.py
@@ -3,7 +3,7 @@ from frankdux import FrankDux
 frank = FrankDux()
 
 # Generate the client library
-# frankdux app:frank
+# frankdux app:frank python
 
 @frank.register(int, int, returns=int)
 def add(first_var, second_var):
@@ -12,6 +12,15 @@ def add(first_var, second_var):
 @frank.register(int, int, returns=int)
 def subtract(a, b):
     return a - b
+
+@frank.register(str, str, returns=str)
+def concat_and_make_fun(a, b):
+    return a + b + " you suck"
+
+@frank.register([str])
+def concat_all(list_of_str):
+    pass
+
 
 if __name__ == "__main__":
     frank.run()
