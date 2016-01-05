9a055863ebcebdc5230bcd028d276d0d2e0316df
Jon Haddad <jon@jonhaddad.com>
2016-01-05 12:50:15 -0800

    class creation seems to work ok, next is nesting

--- frankdux/types.py
+++ frankdux/types.py
@@ -1,8 +1,8 @@
 
 class Descriptor(object):
-
+    name = None
     def __get__(self, instance, owner):
-        pass
+        return instance._values[self.name]
 
     def __set__(self, instance, value):
         pass
@@ -44,19 +44,31 @@ class TypeMetaClass(type):
     def __new__(cls, name, bases, attrs):
         # get a list of all the
         fields = [(name, value) for (name, value) in attrs.items() if isinstance(value, Descriptor)]
+
+        # assign the field names
+        for name, instance in fields:
+            instance.name = name
+
         attrs["_fields"] = set([x[0] for x in fields])
+        attrs["_values"] = {}
+        # ensure each of the metaclass instances knows it's field name
+
         return super(TypeMetaClass, cls).__new__(cls, name, bases, attrs)
 
 
 # Descriptor because we want to have nested types
-class BaseType(Descriptor):
+class BaseType(object):
     _fields = None
+    _values = None
     def __init__(self, **kwargs):
         # check if the fields all exist
         fields = kwargs.keys()
         if len(set(fields) - self._fields) > 0:
             raise TypeError
-        
+
+        for field, value in kwargs.iteritems():
+            self._values[field] = value
+
 
 
 # inherit from this
