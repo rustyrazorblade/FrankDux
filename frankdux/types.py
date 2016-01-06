0cba87e4d24b185b0ba364cd8d6570644e16eb0f
Jon Haddad <jon@jonhaddad.com>
2016-01-05 18:22:14 -0800

    default empty map

--- frankdux/types.py
+++ frankdux/types.py
@@ -10,6 +10,9 @@ class Descriptor(object):
     def __set__(self, instance, value):
         instance._values[self.name] = value
 
+    def _default(self):
+        return None
+
 class Primitive(Descriptor):
     pass
 
@@ -43,20 +46,23 @@ class Map(Collection):
         self._key_type = key_type
         self._value_type = value_type
 
+    def _default(self):
+        return {}
 
 class List(Collection):
-    pass
+    def _default(self):
+        return []
 
 class TypeMetaClass(type):
     def __new__(cls, name, bases, attrs):
         # get a list of all the
-        fields = [(name, value) for (name, value) in attrs.items() if isinstance(value, Descriptor)]
+        fields = {name:value for (name, value) in attrs.items() if isinstance(value, Descriptor)}
 
         # assign the field names
-        for name, instance in fields:
+        for name, instance in fields.iteritems():
             instance.name = name
 
-        attrs["_fields"] = set([x[0] for x in fields])
+        attrs["_fields"] = fields  # k/v list of name:type
         attrs["_values"] = {}
         # ensure each of the metaclass instances knows it's field name
 
@@ -71,11 +77,17 @@ class BaseType(Descriptor):
     def __init__(self, **kwargs):
         # check if the fields all exist
         fields = kwargs.keys()
-        if len(set(fields) - self._fields) > 0:
+        if len(set(fields) - set(self._fields.keys())) > 0:
             raise TypeError
 
-        for field, value in kwargs.iteritems():
-            self._values[field] = value
+        for field in self._fields:
+            # get the value provided in kwargs or use the default
+            # import ipdb; ipdb.set_trace()
+            val = kwargs.get(field, self._fields[field]._default())
+            self._values[field] = val
+
+        # for field, value in kwargs.iteritems():
+        #     self._values[field] = value
 
 
 
