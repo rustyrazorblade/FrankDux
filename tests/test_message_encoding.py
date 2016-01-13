6cdd6f16c1d9be50399fb76083eaf97145e9c0f8
Jon Haddad <jon@jonhaddad.com>
2016-01-12 21:05:53 -0800

    adding encoding to type registry

--- tests/test_message_encoding.py
+++ tests/test_message_encoding.py
@@ -1,7 +1,32 @@
-from frankdux import FrankDux
+from frankdux import TypeRegistry
 from pytest import fixture, raises
+from frankdux.types import Type, Int, String, Map
+
+class SimpleUser(Type):
+    name = String()
+    age = Int()
+
+class Address(Type):
+    street = String()
+    state = String()
+    zip = String()
+
+class User(Type):
+    name = String()
+    age = String()
+    addresses = Map(String, Address)
+
+@fixture
+def registry():
+    r = TypeRegistry()
+    r.add_type(SimpleUser)
+    r.add_type(User)
+
+    return r
 
 
-
+def test_simple_encoding(registry):
+    s = SimpleUser(name="jon", age=34)
+    registry.encode(s)
 
 
